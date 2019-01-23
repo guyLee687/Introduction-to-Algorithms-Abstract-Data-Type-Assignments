4c6bb5ad41cb4d6b5d9c7ffff52ac7227aa4d0b8
jwang358 <jwang358@ucsc.edu>
2019-01-23 01:51:46 -0800

    F inshed raw code for APint

--- CMPS101S18PA1/OverflowHandling.java
+++ CMPS101S18PA1/OverflowHandling.java
@@ -8,9 +8,9 @@ public OverflowHandling
 		private Node<Integer> head;
 		private Node<Integer> tail;
 		private Node<Integer> current = tail;
-		private static size = 0; 
+		private int size = 0; 
 		private int defaultDigits = 8;
-		private int sign = 1;			      //(+ is a 1, - is a 0)	
+		private int realSign, sign = 1;			      //(+ is a 1, - is a -1)	
 
 
 		public IntegerLinkedList()
@@ -20,206 +20,394 @@ public OverflowHandling
 
 		public IntegerLinkedList(Character[] digits)
 		{
-			if(digits[0] == '-')
-				sign == 0;
+			if(digits[0] == '-')			//Might need to revise if digits[0]
+				sign == -1;					//Doesn't contain sign
 
-			Node<Integer> temp = new Node<Integer>(0);
-			tail = temp;
-			for(int i = 1; i < digits.length; i + defaultDigits)
-			{ 
-				int cell;
-				for (int k = 0; k < 8; k++)
-				{
-					cell += digits[k] * (Math.pow(10,k));
+				Node<Integer> temp = new Node<Integer>(0);
+				tail = temp;
+				for(int i = 1; i < digits.length; i + defaultDigits)
+				{ 
+					int cell;
+					for (int k = 0; k < 8; k++)
+					{
+						cell += digits[k] * (Math.pow(10,k));
+					}
+					head = new Node<Integer>(cell);
+					temp.previous = head;
+					head.next = temp;
+					size++;
+					removeLast();
 				}
-				head = new Node<Integer>(cell);
-				temp.previous = head;
-				head.next = temp;
-				size++;
+			}
+
+			public IntegerLinkedList(Character[] digits, int defaultDigits)
+			{
+				if(digits[0] == '-')
+					sign == 0;
+
+				Node<Integer> temp = new Node<Integer>(0);
+				tail = temp;
+				for(int i = 1; i < digits.length; i + defaultDigits)
+				{ 
+					int cell;
+					for (int k = 0; k < 8; k++)
+					{
+						cell += digits[k] * (Math.pow(10,k));
+					}
+					head = new Node<Integer>(cell);
+					temp.previous = head;
+					head.next = temp;
+					size ++;
+				}
 				removeLast();
 			}
-		}
 
-		public IntegerLinkedList(Character[] digits, int defaultDigits)
-		{
-			if(digits[0] == '-')
-				sign == 0;
+			public Integer getFirst()
+			{
+				if (size == 0)
+					return null;
+				else 
+					return head.value;
+			}
 
-			Node<Integer> temp = new Node<Integer>(0);
-			tail = temp;
-			for(int i = 1; i < digits.length; i + defaultDigits)
-			{ 
-				int cell;
-				for (int k = 0; k < 8; k++)
+			public Integer getLast()
+			{
+				if(size == 0)
+					return null;
+				else
+					return tail.value;
+			}
+
+			public void addFirst(Integer digit) 
+			{
+				if(size == 0)
+					head = tail = new Node<Integer>(digit);
+				else if (size == 1)
 				{
-					cell += digits[k] * (Math.pow(10,k));
+					head = new Node<Integer>(digit);
+					head.next = tail;
+					tail.previous = head;
 				}
-				head = new Node<Integer>(cell);
-				temp.previous = head;
-				head.next = temp;
-				size ++;
+				else
+				{
+					Node<Integer> temp = head;
+					head = new Node<Integer>(digit);
+					head.next = temp;
+					temp.previous = head;
+					size++;
+				}
 			}
-			removeLast();
-		}
 
-		public Integer getFirst()
-		{
-			if (size == 0)
+			public void addLast(Integer digit)
+			{
+				if(size == 0)
+					head = tail = new Node<Integer>(digit);
+				else if (size == 1)
+				{
+					tail = new Node<Integer>(digit);
+					tail.previous = head;
+					head.next = previous;
+				}
+				else
+				{
+					Node<Integer> temp = tail;
+					tail = new Node<Integer>(digit);
+					tail.previous = temp;
+					temp.next = tail;
+					size++;
+				}
+			}
+
+			public void removeFirst()
+			{
+				if(size > 1)
+				{
+					head = head.next;
+					head.previous = null;
+				}
+				else if(size == 1)
+					head = tail = null;
+			}
+
+			public void removeLast()
+			{
+				if (!(size > 1))
+				{
+					tail = tail.previous;
+					tail.next = null;
+				}
+				else if(size == 1)
+					head = tail = null;
+			}
+
+			public Integer getPrev()
+			{
+				if(size == 0)
+					return null;
+				else 
+					return tail.previous
+			}
+
+			public int getSize()
+			{
+				return size;
+			}
+
+			public void setCurrent(int index)
+			{
+				if(index == 0)
+					current = head;
+				else if(index = (size - 1))
+					current = tail
+				else
+				{
+					Node<Integer> temp = head;
+					for(int i = 1; i < index; i++)
+						temp = temp.next;
+					current = temp;
+				}
+			}
+
+			public int getCurrent()
+			{
+				if(!current = null)
+					return current.value;
 				return null;
-			else 
-				return head.value;
-		}
-
-		public Integer getLast()
-		{
-			if(size == 0)
-				return null;
-			else
-				return tail.value;
-		}
-
-		public void addFirst(Integer digit) 
-		{
-			if(size == 0)
-				head = tail = new Node<Integer>(digit);
-			else if (size == 1)
-			{
-				head = new Node<Integer>(digit);
-				head.next = tail;
-				tail.previous = head;
 			}
-			else
+
+			public void prevCurrent()
 			{
-				Node<Integer> temp = head;
-				head = new Node<Integer>(digit);
-				head.next = temp;
-				temp.previous = head;
-				size++;
+				current = current.previous;
 			}
-		}
 
-		public void addLast(Integer digit)
-		{
-			if(size == 0)
-				head = tail = new Node<Integer>(digit);
-			else if (size == 1)
+			public void nextCurrent()
 			{
-				tail = new Node<Integer>(digit);
-				tail.previous = head;
-				head.next = previous;
+				current = current.next;
 			}
-			else
+
+			public int getSize()
 			{
-				Node<Integer> temp = tail;
-				tail = new Node<Integer>(digit);
-				tail.previous = temp;
-				temp.next = tail;
-				size++;
+				return size;
 			}
-		}
 
-		public void removeFirst()
-		{
-			if(size > 1)
+			public void setSign(int i)
 			{
-				head = head.next;
-				head.previous = null;
+				sign = i;
 			}
-			else if(size == 1)
-				head = tail = null;
-		}
 
-		public void removeLast()
-		{
-			if (!(size > 1))
+			public void changeSign()
 			{
-				tail = tail.previous;
-				tail.next = null;
+				size *= -1;
 			}
-			else if(size == 1)
-				head = tail = null;
-		}
 
-		public Integer getPrev()
-		{
-			if(size == 0)
-				return null;
-			else 
-				return tail.previous
-		}
-		
-		public int getSize()
-		{
-			return size;
-		}
-
-		public void setCurrent(int index)
-		{
-			if(index == 0)
-				current = head;
-			else if(index = (size - 1))
-				current = tail
-			else
+			public int getSign()
 			{
-				Node<Integer> temp = head;
-				for(int i = 1; i < index; i++)
-					temp = temp.next;
-				current = temp;
+				return sign;
 			}
-		}
-
-		public int getCurrent()
-		{
-			if(!current = null)
-				return current.value;
-		}
-
-		public void prevCurrent()
-		{
-			current = current.previous;
-		}
-
-		public void nextCurrent()
-		{
-			current = current.next;
-		}
 
 		// Revise with sign notations
-		public IntegerLinkedList add(IntegerLinkedList addend)
-		{
-			IntegerLinkedList sum = new IntegerLinkedList()
-			int amount_Of_Calculations;
-			if(size > addend.getSize())
-				amount_Of_Calculations = addend.getSize();
-			else
-				amount_Of_Calculations = size;
-
-			setCurrent(size - 1);
-			addend.setCurrent(size - 1);
-
-			int temp = 0;
-			int limit = Math.pow(10,defaultDigits);
-			for (int i = 0; i < amount_Of_Calculations; i++)
+			public IntegerLinkedList add(IntegerLinkedList addend)
 			{
-				int total = getCurrent() + addend.getCurrent() + temp;
-				if(total > limit)
-					temp %= limit;
-				sum.addFirst(total);
-				prevCurrent();
-				addend.prevCurrent();
+				if (sign == addend.getSign())
+				{ 
+					IntegerLinkedList sum = new IntegerLinkedList();
+					IntegerLinkedList biggerAddend
+					int amount_Of_Calculations;
+					if(size > addend.getSize())
+					{
+						amount_Of_Calculations = addend.getSize();
+						biggerAddend = this;
+					}
+					else
+					{
+						amount_Of_Calculations = size;
+						biggerAddend = addend;
+					}
+
+					setCurrent(size - 1);
+					addend.setCurrent(size - 1);
+
+					int temp = 0;
+					int limit = Math.pow(10,defaultDigits);
+					for (int i = 0; i < amount_Of_Calculations; i++)
+					{
+						int total = getCurrent() + addend.getCurrent() + temp;
+						if(total > limit)
+							temp = total % limit;
+						sum.addFirst(total);
+						prevCurrent();
+						addend.prevCurrent();
+					}
+
+					while(!(biggerAddend.getSize() - amount_Of_Calculations == 0))
+					{
+						biggerAddend.prevCurrent();
+						sum.addFirst(biggerAddend.getCurrent());
+						amount_Of_Calculations++;
+					}
+					addend.setSign(sign);
+					return sum;
+				}
+				else 
+				{
+					addend.changeSign();
+					return subtract(addend)
+				}
+
 			}
 
-		}
-		private static class Node<Integer> 
-		{
-			Node<Integer> next;
-			Integer<Integer> previous;
-			Integer value;
-
-			public Node<Integer>(value)
+			public IntegerLinkedList subtract(IntegerLinkedList subtrahend)
 			{
-				this.value = value; 
+				if (sign == subtrahend.getSign())
+				{
+					IntegerLinkedList diff = new IntegerLinkedList();
+					IntegerLinkedList minuend = this;
+					
+					setCurrent(size - 1);
+					subtrahend.setCurrent(size - 1);
+
+					if(size < subtrahend.getSize())
+					{
+						minuend.changeSign();
+						minuend = subtrahend;
+						subtrahend = this;
+					}
+
+					int temp = 0;
+					int carryOver = Math.pow(10, defaultDigits + 1);
+					for (int i = 0; i < subtrahend.size(); i++)
+					{
+						int difference = (minuend.getCurrent() - temp) - subtrahend.getCurrent();
+						temp = 0;
+						if (difference < 0)
+						{
+							temp++;
+							difference += carryOver;
+						}
+						diff.addFirst(difference);
+						minuend.prevCurrent();
+						subtrahend.prevCurrent();
+					}
+
+					int amount_Of_Calculations = subtrahend.size();
+					while(!(minuend.getSize() - amount_Of_Calculations == 0))
+					{
+						minuend.prevCurrent();
+						diff.addFirst(minuend.getCurrent());
+						amount_Of_Calculations++;
+					}
+					diff.setSign(minuend.getSign());
+					return diff;
+
+				}
+				else
+				{
+					subtrahend.changeSign();
+					return add(subtrahend);
+				}
+			}
+
+			public IntegerLinkedList multiply(IntegerLinkedList factor)
+			{
+				IntegerLinkedList product = new IntegerLinkedList();
+				int position = Math.pow(10, defaultDigits + 1);
+
+				for(int i = 0; i < size; i++)
+				{
+					for(int k = 0; k < factor.getSize(); k++)
+					{
+						IntegerLinkedList placeHolder = new IntegerLinkedList();
+						
+						for(int = l; l <= (i+k); l++)
+							placeHolder.addLast(0);
+						
+						int dig = tempRow * factor.getCurrent();
+						
+						if(dig > position)
+						{
+							placeHolder.addFirst(dig % position);
+							dig %= position;
+						}
+						
+						placeHolder.addFirst(dig);
+						product = product.add(placeHolder)
+					}
+				}
+
+				if(getSign() != factor.getSign())
+					product.changeSign();
+			}
+
+			public IntegerLinkedList divide(IntegerLinkedList divisor)
+			{
+				IntegerLinkedList quotient = new IntegerLinkedList(new IntegerLinkedList(),1);
+				if (divisor.getSize() > size)
+				{
+					quotient.addFirst(0);
+					return quotient
+				}
+				else if (divisor.getSize() == size)
+				{
+					quotient.addFirst(head.value/divisor.getFirst())
+					return quotient;
+				}
+
+				IntegerLinkedList dynamicDividend = new IntegerLinkedList(this, 1);
+				IntegerLinkedList dynamicDivisor = new IntegerLinkedList(divisor, 1);
+				
+				dynamicDividend.setCurrent(0);
+				IntegerLinkedList dynamic = new IntegerLinkedList(new IntegerLinkedList(), 1);
+				
+				for (int i = 0; i <= dynamicDivisor; i++)
+				{
+					dynamic.addLast(dynamicDividend.getCurrent());
+					dynamicDividend.nextCurrent();
+				}
+
+				while(dynamicDividend.currentHasNext())
+				{
+					int count = 0;
+					if(dynamic.CompareTo(divisor) > 1)
+					{
+						dynamic = dynamic.subtract(dynamicDivisor);
+						count++;
+					}
+					else
+					{
+						dynamic.addLast(dynamicDividend.getCurrent());
+						quotient.addLast(count);
+						count = 0;
+					}
+				}
+
+				if(sign != divisor.getSign())
+					quotient.changeSign();
+				return quotient;
+			}
+
+			public Boolean currentHasNext()
+			{
+				if(current.next == null) 
+					return false;
+				return true;
+			}
+
+			public Boolean currentHasPrev()
+			{
+				if(current.previous null)
+					return false;
+				return true;
+			}
+			private static class Node<Integer> 
+			{
+				Node<Integer> next;
+				Integer<Integer> previous;
+				Integer value;
+
+				public Node<Integer>(value)
+				{
+					this.value = value; 
+				}
 			}
 		}
 	}
-}
